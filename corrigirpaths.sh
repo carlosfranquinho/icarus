f94987cecdcc83b2afb4fa202d41fb78a896eec3
Carlos Franquinho <franquinho@gmail.com>
2025-07-15 18:42:21 +0100

    wp-includes corrigidos

--- corrigirpaths.sh
+++ corrigirpaths.sh
@@ -9,7 +9,7 @@ echo "A corrigir paths absolutos dentro da pasta: $PASTA_SITE"
 find "$PASTA_SITE" -type f \( -name "*.html" -o -name "*.css" -o -name "*.js" -o -name "*.xml" \) -print0 |
   while IFS= read -r -d '' ficheiro; do
     echo "Corrigindo: $ficheiro"
-    sed -i 's|"/wp-content|"wp-content|g' "$ficheiro"
+    sed -i 's|"/wp-includes|"wp-includes|g' "$ficheiro"
   done
 
 echo "✅ Substituições concluídas."
